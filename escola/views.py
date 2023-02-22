e5d24429a2701016160aca902b9c602ce79e9d3b
ewerson cordeiro <ewersoncdesouza@gmail.com>
2023-02-22 09:04:59 -0300

    atualização de novas classes

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,generics
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunoMatriculadosSerializer
 
 class AlunoViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas """
@@ -16,3 +16,17 @@ class MatriculaviewSet(viewsets.ModelViewSet):
     """exibindo todas as matriculas"""
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    
+class ListaMatriculasAluno(generics.ListAPIView):
+    """ listando as matriculas de um aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+    
+class listaalunomatriculados(generics.ListAPIView):
+    """listando todos os alunos de um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunoMatriculadosSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunoViewSet, CursoviewSet, MatriculaviewSet
+from escola.views import AlunoViewSet, CursoviewSet, MatriculaviewSet, ListaMatriculasAluno, listaalunomatriculados
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -10,8 +10,9 @@ router.register('cursos', CursoviewSet, basename= 'cursos')
 router.register('matriculas', MatriculaviewSet, basename= 'matriculas')
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls))
+    path('',include(router.urls)),
+    path('aluno/<int:pk>/matriculas/',ListaMatriculasAluno.as_view()),
+    path('curso/<int:pk>/matriculas/',listaalunomatriculados.as_view()),
 ]
